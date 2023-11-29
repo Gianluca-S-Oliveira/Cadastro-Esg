0ca13a607f9c4ae60a3f9569a4fee693b9cb837e
Gianluca Oliveira <gianligth2099@gmail.com>
2023-11-29 15:55:57 -0300

    feat:add home page

--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -1,7 +1,22 @@
 import React, { useState } from "react";
-
+import helloIgh from "../../images/hello.jpg";
+import "./home.scss";
 const Home = () => {
-  return <div id="#">hpome page aee!!</div>;
+  return (
+    <div id="#" className="containerHome">
+      <h1>
+        <span>Olá!</span> Já praticou sua atividade sustentável hoje?
+      </h1>
+      <div className="conntainerImg">
+        {" "}
+        <img src={helloIgh} />
+      </div>
+      <div className="submit">
+        <h2>Não?? Então vamos lá!</h2>
+        <a href="/form">Registrar Atividade </a>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
