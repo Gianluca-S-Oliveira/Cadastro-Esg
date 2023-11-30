94c1c60158ae54ef3795300dddf07cc259957703
Gianluca Oliveira <gianligth2099@gmail.com>
2023-11-30 18:40:47 -0300

    feat:add count and style to list

--- src/Containers/Atividades/index.js
+++ src/Containers/Atividades/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
+import "./atividades.scss";
 
 const Atividades = () => {
   const atividades = JSON.parse(localStorage.getItem("atividades")) || [];
   const [pontos, setPontos] = useState([]);
-
+  var valor = localStorage.getItem("pontos");
+  console.log("valor dos pontos" + valor);
   useEffect(() => {
     const newPontos = atividades.map((atividade) => {
       switch (atividade.tipoAtividade) {
@@ -22,30 +24,50 @@ const Atividades = () => {
 
     setPontos(newPontos);
   }, [atividades]);
-
+  var acrescimo = 0;
   const totalPontos = pontos.reduce((acc, ponto) => acc + ponto, 0);
   localStorage.setItem("pontos", totalPontos);
+
   return (
-    <div id="atividades">
-      pontos atuais:{totalPontos}
-      <h2>Lista de Atividades</h2>
-      {atividades.map((atividade, index) => (
-        <div key={index} style={{ marginBottom: "20px" }}>
-          <strong>Tipo de Atividade:</strong> {atividade.tipoAtividade}
-          <br />
-          <strong>Descrição:</strong> {atividade.descricao}
-          <br />
-          <strong>Localização:</strong> {atividade.localizacao}
-          <br />
-          <strong>Categorias:</strong>{" "}
-          {Object.entries(atividade.categorias).map(
-            ([categoria, valor]) =>
-              valor && <span key={categoria}>{categoria}, </span>
-          )}
-          <hr />
+    <>
+      {valor === "0" ? (
+        <div>
+          <h3 className="">
+            Olá! Você ainda não possui nenhuma atividade registrada :({" "}
+          </h3>
         </div>
-      ))}
-    </div>
+      ) : (
+        <div id="atividades" className="atividadeContainer">
+          {" "}
+          <p className="pontos"> pontos atuais: {totalPontos}</p>{" "}
+          <h2>Lista de Atividades</h2>{" "}
+          {atividades.map((atividade, index) => (
+            <div
+              key={index}
+              style={{
+                marginBottom: "20px",
+              }}
+              className="atividade"
+            >
+              <h3>
+                Atividade: <span className="spanNumber">{index + 1} </span>
+              </h3>
+              <strong>Tipo de Atividade:</strong> {atividade.tipoAtividade}
+              <br /> <strong>Descrição</strong> {atividade.descricao}
+              <br /> <strong>Localização</strong> {atividade.localizacao}
+              <br /> <strong>Categorias</strong>{" "}
+              <div className="">
+                {" "}
+                {Object.entries(atividade.categorias).map(
+                  ([categoria, valor]) =>
+                    valor && <span key={categoria}> {categoria}, </span>
+                )}
+              </div>{" "}
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
